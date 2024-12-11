259ecd689a15e2337185ad7426f0c0aa61073bdf
isikdev <isikjandev@gmail.com>
2024-12-11 11:10:15 +0500

    101%

--- disp/assets/js/script.js
+++ disp/assets/js/script.js
@@ -1,4 +1,12 @@
 let menu = $(".menu")
+let settings = $(".settings")
+let support = $(".support")
 menu.on("click", function () {
-    $(".navbar__menu").toggleClass("navbar__menu-active");
+    $(".navbar__menu-info").toggleClass("navbar__menu-info-active");
+});
+settings.on("click", function () {
+    $(".navbar__menu-settings").toggleClass("navbar__menu-settings-active");
+});
+support.on("click", function () {
+    $(".navbar__settings").toggleClass("navbar__menu-settings-active");
 });
